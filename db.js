ae58fabdbae8ce6b68413eceffa5ed450963ff94
Brian <btran8888@gmail.com>
2015-04-27 05:53:39 -0400

    Changed name of Node Modules

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
 	'url' : 'mongodb://localhost/test'
+	//'url' = process.env.OPENSHIFT_MONGODB_DB_URL;
 }
