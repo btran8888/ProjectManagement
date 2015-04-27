ad7c589f0972628cf7800e38c68adb13244ce0bd
btran8888 <btran8888@gmail.com>
2015-04-27 01:07:51 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
 	//'url' : 'mongodb://localhost/test'
-	'url' = process.env.OPENSHIFT_MONGODB_DB_URL;
+	//'url' = process.env.OPENSHIFT_MONGODB_DB_URL;
+	'url' = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGOHQ_URI || processLAB_URI;
 }
