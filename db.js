05ac858bb4c01cb7e76074d59116c85cf42a6ae7
btran8888 <btran8888@gmail.com>
2015-04-27 01:27:47 -0400

    Update db.js

--- db.js
+++ db.js
@@ -3,5 +3,6 @@ module.exports = {
 	//'url' : 'mongodb://localhost/test'
 	//'url' = process.env.OPENSHIFT_MONGODB_DB_URL;
 	//'url' = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGOHQ_URI || processLAB_URI;
-	'url' = process.env.OPENSHIFT_MONGODB_DB_URL + "/test";
+	//'url' = process.env.OPENSHIFT_MONGODB_DB_URL + "/test";
+	'url' = process.env.MONGOHQ_URI + '/test' || processLAB_URI + '/test';
 }
